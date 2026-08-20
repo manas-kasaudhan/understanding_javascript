37ead7e88959c39ae21cc3cd239b47d992aaa076
manas-kasaudhan <manaskasaudhan1808@gmail.com>
2026-08-20 20:49:59 +0530

    about comparison "==" and "==="

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,32 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2<1);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+// comaprsion should be of 2 similar data types
+
+console.log("2">1); // true because is greater , but these are wrong approach
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // null is converted into 0 
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); // checks the data type
+
+
+
+
+
+
+
