dee43c84863cb9f9d8c8e85694fa5a0f2eb67369
manas-kasaudhan <manaskasaudhan1808@gmail.com>
2026-08-20 15:25:05 +0530

    "About Opreations , Conversion and why it happens in JS"

--- 01_Basics/03_conversionandOpreations.js
+++ 01_Basics/03_conversionandOpreations.js
@@ -24,3 +24,38 @@ let number = 33
 let stringNumber = String(number)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// ******************* Opreations ********************//
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Manas"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // gives output 122
+console.log(1 + 2 + "2"); // gives output 32  because of conversion
+
+let gameCounter = 100 ;
+++ gameCounter; // pre adds 1 to the original value then prints it
+console.log(gameCounter)
+gameCounter++;
+console.log(gameCounter) // adds 1 to the current value and store it 
+
+
+
+
